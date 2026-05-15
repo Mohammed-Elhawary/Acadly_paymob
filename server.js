b3cf6f4b74ebef913ab1fa7b734d8201e99ed063
Mohammed Elhawary <elhawaryh0@gmail.com>
2026-05-16 00:25:16 +0300

    update api payment logic

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(express.json());
 
 app.use("/api", paymentRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+if (require.main === module) {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+module.exports = app;
